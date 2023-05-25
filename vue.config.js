a961e2505aae5842215e4ca5e52658f95bfaa3ae
flystudio <2801193788@qq.com>
2023-05-25 23:54:13 +0800

    项目初始化-调整初始目录结构

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,10 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: {
+    host: 'localhost',
+    //   port: 3000,   //修改项目默认端口
+    open: true
+  },
+  lintOnSave: false // 关闭eslint检查
 })
